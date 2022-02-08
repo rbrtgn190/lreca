321140d41c62bcb4df1544b2fce282f340089532
rbrtgn190 <rbrtgn190@gmail.com>
2022-02-08 16:38:34 +0100

    Created swaggerApi.js

--- petstoreWS/main.js
+++ petstoreWS/main.js
@@ -1,9 +1,12 @@
-load.initialize(async function () {
+// This script was generated and reflects raw data. It is recommended to change this code to your required logic
+
+const swaggerApi = require('./swaggerApi.js')();
+
+load.initialize("Initialize", async function () {
 });
 
 load.action("Action", async function () {
-    load.log("Hello DevWeb");
 });
 
-load.finalize(async function () {
+load.finalize("Finalize", async function () {
 });
